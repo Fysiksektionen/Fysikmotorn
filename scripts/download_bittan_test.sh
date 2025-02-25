847bc5fe8d2fe3babc53aa2f57883aa21b25419d
Leo Trolin <leo.trolin@gmail.com>
2025-02-25 21:08:19 +0100

    Add download_bittan_test.sh

--- scripts/download_bittan_test.sh
+++ scripts/download_bittan_test.sh
@@ -3,4 +3,4 @@ source ../.env
 
 mkdir ../services/bittan_test_frontend 2> /dev/null
 
-install_release Fysiksektionen/bittan build.tar.gz ../services/bittan_test_frontend f0.0.5
+install_release Fysiksektionen/bittan build.tar.gz ../services/bittan_test_frontend $BITTAN_TEST_VERSION
