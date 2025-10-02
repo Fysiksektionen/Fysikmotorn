29e5ea73291f001e9a256d3fbc1e7acd5c12111c
Leo Trolin <leo.trolin@gmail.com>
2025-10-02 21:46:03 +0200

    Move download scripts to project-ops

---
+++ project-ops/fnkth.se/download_fnkthse.sh
@@ -0,0 +1,6 @@
+source ../../download.sh
+source .env
+
+mkdir ../../services/bittan_marke_frontend 2> /dev/null
+
+install_release fysiksektionens-naringslivsnamnd/hemsida build.tar.gz ../../services/fnkth.se $FNKTHSE_VERSION
