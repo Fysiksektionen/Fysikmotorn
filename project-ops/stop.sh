925ec1220c3df9e97d4566b43ca7f18a82dc69e6
Leo Trolin <leo.trolin@gmail.com>
2025-10-02 18:49:13 +0200

    Add project-ops script stop

---
+++ project-ops/stop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Warning: No container name supplied."
+    echo "Usage: $0 <container_name>"
+    exit 1
+fi
+
+cd ../scripts
+source gather_envs.sh
+docker compose down $1
